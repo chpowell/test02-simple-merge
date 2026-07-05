a6d7412cc5db9df8b418e1481d1893c84e9eb85b
Chris Powell <cpowell@eprotege.ca>
2026-07-04 22:25:33 -0700

    Update startup message on main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is my initial program");
+        System.out.println("This is the main branch version");
     }
 }
