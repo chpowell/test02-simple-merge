f7d764f0f408dd1b556c121ecdc274d404e38a44
Chris Powell <cpowell@eprotege.ca>
2026-07-04 22:19:05 -0700

    Add third message on feature branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,5 +7,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is my initial program");
         System.out.println("Feature Branch: added second message");
+        System.out.println("Feature Branch: added third message");
     }
 }
