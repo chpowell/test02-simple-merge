aabf28c64ee9bb8ef78aca797e5336c0c9036e96
Chris Powell <cpowell@eprotege.ca>
2026-07-04 22:01:12 -0700

    Create initial Java project

---
+++ README.md
@@ -0,0 +1,5 @@
+test02-simple-merge 
+- create a main code branch (A), make some changes (B,C)
+- create a branch (F,G)
+- update main (D,E)
+
---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/*
+ * test02-simple-merge
+ * Git branching demonstration.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is my intial program");
+    }
+}
