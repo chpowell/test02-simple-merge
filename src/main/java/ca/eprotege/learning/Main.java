a60569c60ebfb517756ecb5f7e6442b9910a30af
Chris Powell <cpowell@eprotege.ca>
2026-07-06 18:08:45 -0700

    Revised print statements before Maven

---
+++ src/main/java/ca/eprotege/learning/Main.java
@@ -0,0 +1,9 @@
+package ca.eprotege.learning;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Application starting...");
+        System.out.println("Version: 1.0.0-SNAPSHOT");
+        System.out.println("Initialization complete.");
+    }
+}
